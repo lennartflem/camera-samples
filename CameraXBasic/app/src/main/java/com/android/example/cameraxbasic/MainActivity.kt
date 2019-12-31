eeda3770745d709a0b616f6e07428e248afa8755
Martin Zeitler <syslogic23@gmail.com>
2019-12-31 05:26:46 +0100

    brackets added.

--- CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/MainActivity.kt
+++ CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/MainActivity.kt
@@ -71,9 +71,13 @@ class MainActivity: AppCompatActivity() {
         fun getOutputDirectory(context: Context): File {
             val appContext = context.applicationContext
             val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
-                File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() } }
-            return if (mediaDir != null && mediaDir.exists())
-                mediaDir else appContext.filesDir
+                File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() }
+            }
+            return if (mediaDir != null && mediaDir.exists()) {
+                mediaDir
+            } else {
+                appContext.filesDir
+            }
         }
     }
 }
